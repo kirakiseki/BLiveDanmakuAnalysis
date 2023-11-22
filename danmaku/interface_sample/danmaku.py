345354572b71ace2076545d67f5b1a172d10ed72
Ishirai <kirakiseki@163.com>
2023-11-22 15:22:06 +0800

    feat: login/connect to live room/watch for danmaku and gifts

---
+++ danmaku/interface_sample/danmaku.py
@@ -0,0 +1,22 @@
+{
+    'room_display_id': 55,
+    'room_real_id': 21144080, 
+    'type': 'DANMU_MSG',
+    'data': 
+        {
+            'cmd': 'DANMU_MSG',
+            'info': 
+                [
+                    [0, 1, 25, 16777215, 1700635324085, 1161989939, 0, 'd7870e65', 0, 0, 0, '', 0, '{}', '{}', {'mode': 0, 'show_player_type': 0, 'extra': '{"send_from_me":false,"mode":0,"color":16777215,"dm_type":0,"font_size":25,"player_mode":1,"show_player_type":0,"content":"谁在黑易峥？","user_hash":"3615952485","emoticon_unique":"","bulge_display":0,"recommend_score":3,"main_state_dm_color":"","objective_state_dm_color":"","direction":0,"pk_direction":0,"quartet_direction":0,"anniversary_crowd":0,"yeah_space_type":"","yeah_space_url":"","jump_to_url":"","space_type":"","space_url":"","animation":{},"emots":null,"is_audited":false,"id_str":"2ad68f1bbd479ba225591b7b10655da258","icon":null,"show_reply":true,"reply_mid":0,"reply_uname":"","reply_uname_color":"","hit_combo":0}'}, {'activity_identity': '', 'activity_source': 0, 'not_show': 0}, 0],
+                    '谁在黑易峥？',
+                    [1227180840, '呜呼呼444', 0, 0, 0, 10000, 1, ''], 
+                    [], 
+                    [0, 0, 9868950, '>50000', 0], 
+                    ['', ''], 
+                    0, 0, None, 
+                    {'ts': 1700635324, 'ct': '620CF131'}, 
+                    0, 0, None, None, 0, 7, [0], None
+                ],
+            'dm_v2': ''
+        }
+}
---
+++ danmaku/main.py
@@ -0,0 +1,94 @@
+import os
+import logging
+import sys
+from dotenv import load_dotenv
+from datetime import datetime
+from bilibili_api import sync, settings, live, user, Credential
+from bilibili_api.login import login_with_password, Check
+
+
+class Logger:
+    def __init__(self, name: str, level: str = "info"):
+        formatter = logging.Formatter(
+            "%(levelname)s - %(asctime)s — %(name)s: %(message)s"
+        )
+
+        self.logger = logging.getLogger(name=name)
+        if level.lower() == "info":
+            self.logger.setLevel(level=logging.INFO)
+        else:
+            self.logger.setLevel(level=logging.DEBUG)
+
+        output_handler = logging.StreamHandler(sys.stdout)
+        output_handler.setFormatter(formatter)
+        self.logger.addHandler(output_handler)
+
+        self.logger.propagate = False
+
+    def getLogger(self):
+        return self.logger
+
+
+def init():
+    global logger
+
+    load_dotenv()
+
+    logger = Logger(name="Danmaku Backend", level="info").getLogger()
+
+    settings.geetest_auto_open = False
+
+
+def login() -> Credential:
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
+
+    if not username or not password:
+        logger.fatal("username or password undefined! Exiting...")
+        exit(1)
+
+    c = login_with_password(username, password)
+
+    if isinstance(c, Check):
+        logger.fatal("Need 2fa login! Exiting...")
+        exit(-1)
+    else:
+        credential = c
+        if not credential.has_buvid3():
+            credential.buvid3 = "2F7CC600-E511-9348-8699-E086CCBC620C74007infoc"
+
+    return credential
+
+
+def connect(room_num: int, credential: Credential):
+    global room
+    room = live.LiveDanmaku(room_num, credential=credential)
+
+    @room.on('DANMU_MSG')
+    async def on_danmaku(event):
+        user = event['data']['info'][2][1]
+        msg = event['data']['info'][1]
+        timestamp = event['data']['info'][0][4]
+        timestr = datetime.fromtimestamp(timestamp / 1000).strftime("%H:%M:%S.%f")[:-3]
+        logger.info(f"{timestr} Received danmaku from {user}: {msg}")
+
+    @room.on('SEND_GIFT')
+    async def on_gift(event):
+        user = event['data']['data']['uname']
+        gift_name = event['data']['data']['giftName']
+        gift_price = event['data']['data']['price']
+        num = event['data']['data']['num']
+        timestamp = event['data']['data']['timestamp']
+        timestr = datetime.fromtimestamp(timestamp).strftime("%H:%M:%S.%f")[:-3]
+        logger.info(f"{timestr} Received gift: {gift_name} x{num} from {user}, value: {num * gift_price}")
+
+    sync(room.connect())
+
+
+if __name__ == '__main__':
+    init()
+    logger.info("Danmaku Backend starting...")
+    logger.info("Trying to login")
+    credential = login()
+    logger.info(f"Logged in: Welcome {sync(user.get_self_info(credential))['name']}")
+    connect(55, credential)
